17a17ff045fa1e643cd7216bce30e6ccd81a9fe0
shahinali-dev <shahinali.dev@gmail.com>
2026-02-09 13:53:00 +0600

    feat: Implement Favicon

--- src/layouts/RootLayout.ts
+++ src/layouts/RootLayout.ts
@@ -245,21 +245,21 @@ const RootLayout = (props: RootLayoutProps) => html`
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/assets/favicon/apple-touch-icon.png"
+        href="/favicon/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/assets/favicon/favicon-32x32.png"
+        href="/favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/assets/favicon/favicon-16x16.png"
+        href="/favicon/favicon-16x16.png"
       />
-      <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
 
       <!-- FontAwesome CSS -->
       <link
